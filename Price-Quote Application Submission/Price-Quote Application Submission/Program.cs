166894c55419883a9a1d7c293bcb28927442ad74
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-10-24 13:07:27 -0700

    Price-Quote Application Assignment Submission
    Your job is to create a console-based application for getting a shipping quote for a package 
    Requirements
    
    The first line of the program must be: “Welcome to Package Express. Please follow the instructions below.”
    The user must then be prompted for the package weight.
    If the weight is greater than 50, display the error message, “Package too heavy to be shipped via Package Express. Have a good day.” At this point the program will end.
    The user must then be prompted for:
    the package width,
    then the package height,
    then the package length.
    If the total of all dimensions is greater than 50, display the error message, “Package too big to be shipped via Package Express.” At this point the program will end.
    Next, multiply the three dimensions (height, width, & length) together, and multiply the product by the weight. Finally, divide the outcome by 100.
    The result of this calculation is the price quote.
    Display the quote to the user as a dollar amount.

---
+++ Price-Quote Application Submission/Price-Quote Application Submission/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_Quote_Application_Submission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //                                                         Price-Quote Application Submission
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below. \n");
+            Console.WriteLine("\n Please Enter Package Weight in Pounds. \n");
+            
+            
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            int weightLimit = 50;
+
+            if (packageWeight > weightLimit)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day. \n"); 
+                Console.ReadLine();
+            }
+            else if (packageWeight < weightLimit)
+            {
+                Console.WriteLine("Please Enter Package width. \n");
+                int pkgWidth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter Package Height. \n");
+                int pkgHeight = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter Package Length. \n");
+                int pkgLength = Convert.ToInt32(Console.ReadLine());
+
+                int totalDimension = Convert.ToInt32 (pkgWidth + pkgHeight + pkgLength);
+
+                if (totalDimension > weightLimit)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. ");
+                }
+                else if (totalDimension < weightLimit)
+                {
+                    int totalSize = (pkgWidth * pkgHeight * pkgLength); 
+                    
+                    decimal priceQuote = totalSize * packageWeight / 100; 
+                    
+                    Console.WriteLine("Your Price Quote is \n" + priceQuote);
+                    Console.ReadLine();
+                }
+
+            }
+
+        }
+    }
+}
