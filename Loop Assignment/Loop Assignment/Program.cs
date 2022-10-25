2ca056400575bbe6393683bba278dc474e0d9de2
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-10-25 11:34:27 -0700

    do-while-Loop-Assignment
    AOLCC Submission
    code that does the following:
    1. Do a Boolean comparison using a “while” statement.
    2. Do a Boolean comparison using a “do while” statement.

---
+++ Loop Assignment/Loop Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Hello, Welcome to My while and do while Loop Statements. \n");
+             int e = 0;
+            while (e < 7)
+            {
+                Console.WriteLine(e);
+                e++;
+            }
+            Console.ReadLine();
+
+
+
+
+            // Unsure by intructions if you wanted a seperate statement for each 
+
+ // int x = 0;
+           //do
+           // {
+           //      Console.WriteLine(x);
+           //     x++;
+           // }
+           //while (x < 12);
+           // Console.ReadLine();
+        }
+    }
+}
