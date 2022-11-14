6438d92db05b176db904e0570ed1292e78bb7525
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-14 10:39:27 -0800

    Calling Methods Assignment
    Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application.
    
    In the Main() program, ask the user what number they want to do the math operations on.
    
    Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Calling Methods/Calling Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Printing greetings to the Main program
+            Console.WriteLine(" Welcome to the ... Magic Math Show ?!? \n " );
+            // end of greeting
+           
+            // Asking for user input
+            Console.WriteLine(" What Number would you like to do an Magic trick on ? ");
+            int myMethodCall = Convert.ToInt32(Console.ReadLine());
+
+            // call my methods to the main prgram to do math operations on
+
+            // Calling my Object to the Main Program 
+            MathAintMathin math = new MathAintMathin();
+
+            // Calling Method 1 to Multiply User Input by 18
+            Console.WriteLine(" Your Math  Return Value for Method 1 is ");
+            Console.WriteLine(MathAintMathin.Method1() * myMethodCall);
+
+
+            // Calling Method 2 to Minus 10 from User Input
+            Console.WriteLine(" Your Math  Return Value for Method 2 in ");
+            Console.WriteLine(math.Method3() - myMethodCall);
+
+
+            //Calling Method 3 to Add +77 to the Users Input
+            Console.WriteLine(" Your Math  Return Value for Method 3 is ");
+            Console.WriteLine(math.Method3() + myMethodCall);
+
+            Console.ReadLine(); // I know there is a prettier way to write this code but my maffs Aint Maffin :)
+        }
+    }
+}
---
+++ Calling Methods/Calling Methods/StrCards.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    public class MathAintMathin
+    {
+        //  assigning values and methods to my  Object class 
+        // i need to create 2 more methods in the same class?
+
+        public  MathAintMathin() // Building a Class , with 3 methods 
+        {
+
+            Console.WriteLine(" Call me the Maffs Master \n ");
+
+        }
+
+        public static int Method1()  // class Method one 
+        {
+            int maffsOne = 18;   // adding value to my  Method Object, i know thats not the correct termonology 
+            return maffsOne;
+           
+        }
+
+        public static int Method2() // class method 2 
+        {
+            int myMaffs = 10;  // Adding value to my Object
+            return myMaffs;
+
+        }
+
+
+
+        public int Method3()    // Adding value to my Object
+        {
+
+            int maffMaster = 77;
+            return maffMaster;
+        }
+
+
+
+        // Setting class Properties
+        public int maffsOne { get; set; } 
+        public int userInputMaffs { get; set; } 
+
+        public int myMaffs { get; set; }
+        public int userMaffs { get; set; }
+
+        public int maffMaster { get; set; }
+        public int InputMaster { get; set; }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
