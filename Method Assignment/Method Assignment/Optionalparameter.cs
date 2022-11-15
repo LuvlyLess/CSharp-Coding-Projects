2615e15d30ca78537fbab1fa8002d13f874bb843
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-15 10:08:00 -0800

    Method Assignment
    create a console app that includes the following:
    
    Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return an integer result.
    
    In the Main() method of the console app, instantiate the class.
    
    Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
    
    Call the method in the class, passing in the one or two numbers entered.
    
    Try various combinations of numbers on the code, including having no second number.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Method Assignment/Method Assignment/Optionalparameter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    public class Optionalparameter //  creating a class that takes two integers with one being optional
+    {
+      
+        public static int Optparam1(int a, int b = 1) // here i have created a parameter to accept  an optional param to be entered
+        {
+            Console.WriteLine("Enter a number: "); // asking use for input
+            int userInput = Convert.ToInt32(Console.ReadLine()); // creating a instance of the user input
+            int x = 52; // created a variable for x  to be used later
+
+            Console.WriteLine("Pick another number optional ");// here is where i created the input/ output for the optional Parameter using an if loop
+            int OptInput = Convert.ToInt32(Console.ReadLine());
+            if (OptInput == 0) // if user input = 0 
+            {
+                Console.WriteLine(" No number entered \n"); // the console will wright  no number entered
+                return a + x; 
+            }
+            else    // here it will continue on with the program doing a math operation 
+            {
+                Console.WriteLine("You numbers Plus 52 Equals ");
+                 return a + b + x;
+            }
+           
+        }
+        
+   } 
+}
---
+++ Method Assignment/Method Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Method_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Optionalparameter Optparam = new Optionalparameter(); // creating an instance of my class
+            Console.WriteLine(Optionalparameter.Optparam1(1)); // calling my class to the main() program to be used 
+
+        }
+
+       
+    }
+}
