9cd8bc895821e4f9ab426a0cac4573a88897903b
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-14 17:54:37 -0800

    Main Method Assignment
    Create a console app that includes the following
    Create a class. In that class, create a method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.), then return the answer as an integer.
    
    In the Main() method of the console app, instantiate the class and call the one method, passing in an integer. Display the result to the screen.
    
    Add a second method to the class with the same name that will take in a decimal, create a different math operation for it, then return the answer as an integer.
    
    In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
    
    Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it, then return the answer as an integer.
    
    In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Main_Method_Assignment/Main_Method_Assignment/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    public class Class1
+    {
+        public int Method1() // here i have created my first Method() to be used or called from the main program
+        {
+            Console.WriteLine("I'm adding 23 to your: user input \n");
+            Console.WriteLine("Please pick a number ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int addingTo = 23;
+            return addingTo + userInput;
+        }
+
+
+        public float Method1(float x, float y ) // here i have created a second Method() that multiplies Your Input number by 18% and returns the Value
+            // think i wanna add an exception message here
+        {
+            
+            Console.WriteLine("Please pick a number ");
+            float userInput2 = Convert.ToSingle(Console.ReadLine());
+            float Percent = 0.18f; // this is the same as 18%
+            
+            float Amount = userInput2 * Percent;
+
+            return Amount;
+        }
+
+        public string Method1(string value) // creating a third class with parameters that converts a string to int 
+        {
+
+            string str = "100 Plus";
+
+            try // i used a try catch for format exception
+            {
+                int x = Convert.ToInt32(str);
+                Console.WriteLine(x);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Input string is invalid.");
+            }
+            return str + 10;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Main_Method_Assignment/Main_Method_Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // a greeting message to start of the program
+            Console.WriteLine("Greeting message here? \n");
+
+
+            // creating an instance of my object
+            Class1 classOne = new Class1();
+
+
+            // Here is where i instantiate the class and call the methods()
+            Console.WriteLine(classOne.Method1());
+            Console.WriteLine(classOne.Method1(1, 2)); 
+            Console.WriteLine(classOne.Method1("value"));
+            Console.ReadLine();
+        }
+    }
+}
