be12a33f1e296d920eab531a6dfd27d3edee25c4
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-15 13:38:09 -0800

    Method Class Assignment
    create a console app that includes the following:
    
    Create a class. In that class, create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen. 
    
    In the Main() method of the console app, instantiate the class.
    
    Call the method in the class, passing in two numbers. 
    
    Call the method in the class, specifying the parameters by name.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using Method_Class_Assignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass MyClass = new MyClass(); // instantiate my class in Main() program
+            MyClass.VoidMethod1(FirstNumber: 1, SecondNumber: 1); // Calling Class Method by name
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Method Class Assignment/Method Class Assignment/myClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    public class MyClass
+    {
+        
+        public static void VoidMethod1(int FirstNumber, int SecondNumber) // creating named parameters to be called by name on Main() program
+        {
+            Console.WriteLine( FirstNumber + 4 + " int X = 1 plus Four Equals five");
+            Console.WriteLine("Displaying second int y Equals " + SecondNumber);   
+        }
+
+        internal void VoidMethod1(object y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
