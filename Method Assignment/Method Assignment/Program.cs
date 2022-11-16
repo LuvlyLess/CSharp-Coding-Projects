2856de107ebe40551537dd6362fbd09ec3dd9340
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-15 16:01:51 -0800

    Update Program.cs

--- Method Assignment/Method Assignment/Program.cs
+++ Method Assignment/Method Assignment/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -15,10 +15,26 @@ namespace Method_Assignment
         {
             
             Optionalparameter Optparam = new Optionalparameter(); // creating an instance of my class
-            Console.WriteLine(Optionalparameter.Optparam1(1)); // calling my class to the main() program to be used 
+            
+            Console.WriteLine("Enter a number: "); // asking use for input
+            int value1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" Enter another number optional "); // asking use for input
+
+            try // using a trying catch to check if user input a number
+            {
+                int value2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Optionalparameter.Optparam1(value1 + value2));
+            }
+            catch // if user entered only one number call class  but only pass one number
+            {
+                Console.WriteLine(Optionalparameter.Optparam1(value1));
+            }
+            Console.ReadLine();
+            
 
         }
 
-       
+
     }
 }
