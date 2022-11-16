8cf3b0861b7394f7e9e47541daba13705bf215d1
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-16 11:30:03 -0800

    Class Method Assignment
    create a console app that includes the following:
    
    Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
    
    In the Main() method, instantiate that class.
    
    Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two. 
    
    Create a method with output parameters.
    
    Overload a method.
    
    Declare a class to be static.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ ClassMethodAssignment2/ClassMethodAssignment2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            VoidClass InstantiateClass = new VoidClass(); // creating my class object in main program
+            Console.WriteLine("Enter a number");  // asking for user input to console
+            int UserInput = Convert.ToInt32(Console.ReadLine());  // taking user input and creating a object from it, UserInput = the users Input
+
+           
+
+            // Calling each method I created in  my void method class
+            InstantiateClass.Method(UserInput); //Calling my Method to use it, on the user input 
+            
+            
+            // calling my second method with int parameters
+            Console.WriteLine(InstantiateClass.MethParams(5, 5));
+
+
+            //Calling my third method Overload Method, it has the same name with different parameters ie (int and float)
+            Console.WriteLine(InstantiateClass.MethParams((int)5.75F, (int)3.75F));
+
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassMethodAssignment2/ClassMethodAssignment2/Void Method Class.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment2
+{
+    public class VoidClass
+    {
+
+
+        public void Method(int x )
+        {
+            
+            int result = x / 2;
+            Console.WriteLine(result);
+                
+                return;   
+        }
+
+
+        public int MethParams(int t, int r)
+        {
+            int Sum = t + r;
+            return Sum + t + r;
+        }
+
+        public float MethParams(float x, float y)
+        {
+            return x * y;
+        }
+
+    }
+}
