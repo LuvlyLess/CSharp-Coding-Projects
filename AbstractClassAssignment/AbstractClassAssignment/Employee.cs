10054ce3bbc32cbe42f2ac60b3d9bb43d07aab19
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-17 12:42:30 -0800

    Abstract Classes
    console app that includes the following:
    
    Create an abstract class called Person with two properties: string firstName and string lastName.
    
    Give it the method SayName().
    
    Create another class called Employee and have it inherit from the Person class.
    
    Implement the SayName() method inside of the Employee class.
    
    Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. Call the SayName() method on the object.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public class Employee : Person // creating a employee class that inherits its properties from the abstract Class Person
+    {
+        public override void SayName() 
+        {
+            base.SayName();
+        }
+
+
+        public override void Name()   // using override keyword to define the method the abstract method 
+        { 
+             throw new NotImplementedException();
+        }
+    }
+}
---
+++ AbstractClassAssignment/AbstractClassAssignment/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public abstract class Person // creating an abstract class with two properties for my class Person
+    {
+        public string firstName { get; set; } // setting properties
+        public string lastName { get; set; }
+        
+        
+
+        public abstract void Name(); // adding a abstract method that states any class inheriting this class must implement this method
+        // using a virtual allows you to override this method in its child classes 
+        public virtual void SayName() // the method that must be called in any child class that inherates from class person 
+        {
+            Console.WriteLine(firstName + " " + lastName);  // void method out put 
+        }
+
+        
+
+
+
+        
+    }
+}
