76858e662fa512a5391b0dd4354c0d1b4a6cdb04
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-18 08:53:30 -0800

    Polymorphism Assignment
    create a console app that includes the following:
    
    Create an interface called IQuittable and have it define a void method called Quit().
    
    Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
    
    Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.

---
+++ Polymorphismabstract assignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{                         // implementing my interface
+    public class Employee : Person, IQuittable // creating a employee class that inherits its properties from the abstract Class Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public override void Name()   // using override 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Quit() 
+        {
+            Console.WriteLine("Fake Quit: Option ");
+            throw new NotImplementedException();
+        }
+        
+
+
+    }
+}
---
+++ Polymorphismabstract assignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)// Updated  Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+        {
+
+            Employee employee = new Employee(); // instatiating my object
+            employee.firstName = "Sample"; // initilizing the value of its properties
+            employee.lastName = "Student";
+            employee.SayName(); // calling my method
+
+            // using polymorphism to create an object and call the Quit() method on it.
+            employee.Quit();
+
+            Console.ReadLine();
+        }   
+
+    }
+}
