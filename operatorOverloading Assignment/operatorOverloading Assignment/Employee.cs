22c4329682189621d4e317bcf9d7589ac61f49dc
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-21 10:18:09 -0800

    Operators Assignment
     create a console app that includes the following:
    
    Create an Employee class with Id, FirstName and LastName properties. 
    In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property. Remember that comparison operators must be overloaded in pairs.

---
+++ operatorOverloading Assignment/operatorOverloading Assignment/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorOverloading_Assignment
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+        // operator overloading is the overloading of the built in operators in C#.
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+
+            if  (emp1.Id == emp2.Id) // using (if) to see if the employee.Id match               
+            {
+                return true;
+            }
+            return false;
+
+        }
+        public static bool operator !=(Employee emp1, Employee emp2) 
+        {
+
+            if (emp1.Id == emp2.Id) // using (if) to see if the employee.Id match               
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+
+    }
+
+
+    
+}
---
+++ operatorOverloading Assignment/operatorOverloading Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorOverloading_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // creating two employees to compare .Id properties
+            Employee emp1 = new Employee();
+            emp1.Id = Convert.ToString(1);
+            emp1.FirstName = "Chester";
+            emp1.LastName = "Bennington";
+
+            Employee emp2 = new Employee();
+            emp2.Id = Convert.ToString(12);
+            emp2.FirstName = "John";
+            emp2.LastName = "MacAfee";
+
+            if (emp1.Id == emp2.Id) // using if to  write response if == || != a match 
+            {
+                Console.WriteLine("The Id Matches");
+            }
+            else
+            {
+                Console.WriteLine("The Id does not match");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
