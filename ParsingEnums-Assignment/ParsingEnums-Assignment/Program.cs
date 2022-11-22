59976636fed388770e9d6d122dd1b774a45268eb
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-22 10:46:12 -0800

    Parsing Enums
    create a console app that includes the following:
    
    Create an enum for the days of the week.
    
    Prompt the user to enter the current day of the week.
    
    Assign the value to a variable of that enum data type you just created.
    
    Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs.

---
+++ ParsingEnums-Assignment/ParsingEnums-Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try // wraping statement in try catch
+            {
+
+                Console.WriteLine("Please enter current day of the week");
+                string day = Console.ReadLine();
+
+                var day2 = (DayOftheWeek)Enum.Parse(typeof(DayOftheWeek), day); //Comparing the user input to the enum values
+                Console.WriteLine(day2 + " is a day of the week");
+
+            }
+            catch
+            {
+                Console.WriteLine("Please Enter an actul day of the week");
+                //  having it print "Please enter an actual day of the week.” to the console if an error occurs.
+            }
+
+            Console.ReadLine();
+        
+
+    }
+        public enum DayOftheWeek // declaring my Enum List DaysOftheWeek, A list of constant values and variables
+        {
+            // Monday = 1;  // if you do not define the "index Number" it will always start at zero Monday,  would equal 0 "index number"
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
