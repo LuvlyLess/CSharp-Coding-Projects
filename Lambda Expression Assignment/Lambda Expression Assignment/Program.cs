aca82a8237501c93e43b6f240b44a4850d94f12b
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-22 15:51:39 -0800

    Lambda Expression Assignment
     create a console app that includes the following:
    
    Create an Employee class with the following properties:
    
    a. Id
    
    b. First Name
    
    c. Last Name
    
    In the Main() method, create a list of at least 10 employees. At least two employees should have the first name “Joe”.
    
    Using a foreach loop, create a new list of all employees with the first name “Joe”. In your comparison statement, remember to reference the property of the object you are checking.
    
    Perform the same action again, but this time with a lambda expression.
    
    Using a lambda expression, make a list of all employees with an Id number greater than 5.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Lambda Expression Assignment/Lambda Expression Assignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Lambda_Expression_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();// Employee object
+
+
+           List<Employee> empList = new List<Employee>(); // creating my employee List(emplist) and giving it value of the Class Employee
+            empList.Add(new Employee() { FirstName = "Joe", Id = 1, LastName = "Smith" }); // Giving my List of employees values for each property
+            empList.Add(new Employee() { FirstName = "Cris", Id = 2, LastName = "Benny" });
+            empList.Add(new Employee() { FirstName = "Sally", Id = 3, LastName = "Eggston" });
+            empList.Add(new Employee() { FirstName = "Jesse", Id = 4, LastName = "Strumin" });
+            empList.Add(new Employee() { FirstName = "Joe", Id = 5, LastName = "Carter" });
+            empList.Add(new Employee() { FirstName = "Sue", Id = 6, LastName = "Chaslin" });
+            empList.Add(new Employee() { FirstName = "Johanson", Id = 7, LastName = "Truman" });
+            empList.Add(new Employee() { FirstName = "Opera", Id = 8, LastName = "Chance" });
+            empList.Add(new Employee() { FirstName = "Harvey", Id = 9, LastName = "Black" });
+            empList.Add(new Employee() { FirstName = "Joel", Id = 10, LastName = "Dene" });
+
+            // Using foreach Loop
+            foreach (var emp in empList) // creating a foreach/for loop to step through my List Employee and find matching Name of "Joe"
+            {
+                if (emp.FirstName == "Joe")
+                {                  
+                    List<Employee> empJoe = new List<Employee>();// creating my List of Joe's
+                    empJoe.Add(emp); // Adding the values"Joes" to my list
+                   Console.WriteLine(emp.FirstName + " " + emp.LastName + " " + emp.Id); // printing each property of each Joe to the console
+                }
+                
+            }
+            // Using Lambda function
+            List<Employee> employeeList2 = empList.Where(emp => emp.Id > 5).ToList(); // Using lambda i checked the list to find how many employees hav an ID # above 5
+
+            Console.WriteLine(employeeList2.Count);// here i printed the value"Count" of the lambda function 
+
+
+            Console.ReadLine();
+        }
+    }
+}
